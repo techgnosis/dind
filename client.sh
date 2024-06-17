b5a08f0f4039b28bc167aadc0a825498a0a61c2a
James Musselwhite <james.musselwhite@astronomer.io>
2024-06-17 15:14:48 -0700

    streamline

--- client.sh
+++ client.sh
@@ -2,9 +2,10 @@
 
 set -euo pipefail
 
+./build.sh
+
 docker run --rm -it --network dind-network \
 -e DOCKER_TLS_CERTDIR=/certs \
 -v dind-certs-client:/certs/client:ro \
--w /root \
+-w /root/astro-project \
 dind-astro-test:1 sh
-# docker:26.1.4 sh
