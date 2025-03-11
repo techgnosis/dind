9d72c785190d939ed934dc85cfc85896feb12309
James Musselwhite <james.musselwhite@astronomer.io>
2025-03-11 13:20:50 -0700

    no hardcoded IDs

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 set -euo pipefail
 
-docker build -t images.astronomer.cloud/clrv6fg0500ab01lv2dvwuj4b/cm84u9eol01cg01me4i88ej04:deploy-2025-03-11T15-45-00 astro-project
+DATE=$(date -u +"%Y-%m-%dT%H-%M-%S")
+
+docker build -t images.astronomer.cloud/$ORG_ID/$DEPLOYMENT_ID:deploy-$DATE astro-project
